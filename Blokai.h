5d4d57fa8cfb27031bb1e2e74c6e2d1c1038958d
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-10-29 12:13:39 +0200

    Add files via upload

---
+++ Blokai.h
@@ -0,0 +1,360 @@
+#include <iostream>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <numeric>   
+#include <unordered_map>      
+using namespace std;
+
+struct Vartotojas {
+    string vardas;
+    string viesasis_raktas;
+    int balansas;
+};
+
+struct Transakcija {
+    string transakcijos_id;
+    string siuntejo_viesasis_raktas;
+    string gavejo_viesasis_raktas;
+    int suma;
+};
+
+struct Blokas {
+    string bloko_id;
+    vector<Transakcija> transakcijos;
+    int nonce;
+};
+
+string randomVardas() {
+    vector<string> var = {"Tomas", "Andrius", "Algirdas", "Lukas", "Povilas", "Vilius", "Justinas", "Martynas", "Kestutis", "Juozas",
+    "Julius", "Rimvydas", "Darius", "Vytis", "Egidijus", "Karolis", "Simonas", "Deividas", "Dominykas", "Edgaras", "Aurimas", "Ricardas", 
+    "Paulius", "Arvydas", "Antanas", "Jurgis", "Adomas", "Gediminas", "Arturas", "Eimantas", "Raimondas", "Arnas", "Eugenijus", "Kostas", 
+    "Birute", "Irena", "Rasa", "Asta", "Lina", "Diana", "Jolanta", "Agne", "Egle", "Inga", "Dovile", "Gabija", "Greta", "Monika", "Vaida", 
+    "Viktorija", "Sandra", "Aurelija", "Rima", "Laima", "Neringa", "Renata", "Justina", "Kristina", "Agne", "Ieva", "Indre", "Marija"};
+
+    vector<string> pav = {"Kazlauskas", "Petrauskas", "Jankauskas", "Zukauskas", "Pavardenis", "Butkus", "Vasiliauskas",
+    "Urbonas", "Jonaitis", "Brazaitis", "Balciunas", "Tamulionis", "Kairys", "Saulys", "Grigas", "Vaitkus", "Stankevicius", "Dauksys", 
+    "Navickas", "Zilinskas", "Mockus", "Sernas", "Leipus","Vilkas", "Kavaliauskas", "Rimkus", "Matulis", "Sakalauskas", "Girdvainis", "Simutis"};
+    
+    string vard = var[rand() % var.size()];
+    string pavard = pav[rand() % pav.size()];
+    return vard + " " + pavard;
+}
+
+string hexPadarymas(bitset<256>& bitai) {
+    stringstream ss;
+
+    for (int i = 0; i < 256; i += 4) // Paemame po 4 bitus is 256 tam, kad konvertuoti 4 bitus i 1 hex
+    {
+        int reiksme = 0; 
+        reiksme += bitai[i] * 8; 
+        reiksme += bitai[i + 1] * 4;
+        reiksme += bitai[i + 2] * 2;
+        reiksme += bitai[i + 3] * 1;
+
+        ss << hex << reiksme;
+    }
+    return ss.str();
+}
+
+void pakeitimasPo16bitus(bitset<256>& bitai, int reiksme_1, int reiksme_2) {
+    for (int i = 0; i < 16; i++) {
+        bool laikinas = bitai[reiksme_1 + i];
+        bitai[reiksme_1 + i] = bitai[reiksme_2 + i]; 
+        bitai[reiksme_2 + i] = laikinas;  
+    }
+}
+
+void bitaiKeiciasiVietomis(bitset<256>& bitai) {
+    int binarinis_ilgis = bitai.size(); 
+    for (int i = 0; i < binarinis_ilgis / 2; i++) 
+    // Dalinu is 2, nes jei turime 10 bitu, tai sukeiciama pagal toki principa: 1-10, 2-9, 3-8, 4-7, 5-6
+    {
+        int laikinas = bitai[i];
+        bitai[i] = bitai[binarinis_ilgis - i - 1]; 
+        bitai[binarinis_ilgis - i - 1] = laikinas;
+    }
+
+    for (int i = 0; i < binarinis_ilgis; i++) {
+        if (bitai[i] == 1) { 
+            bitai[(i + 15) % 256].flip();    
+        }
+    }
+    
+    // Bandymas sugeneruoti daugiau 1 ir 0
+    for (int i = 0; i < binarinis_ilgis / 2; i++) {
+        if (bitai[i] == bitai[binarinis_ilgis / 2 + i]) { // Patikriname ar pirmoji puse kodo lygi su kitaja
+            bitai[i] = 1; // Jeigu taip - rasome 1
+        } else {
+            bitai[i] = 0; // Jeigu ne - rasome 0
+        }
+    }
+    
+    pakeitimasPo16bitus(bitai,  0, 256);
+    pakeitimasPo16bitus(bitai, 16, 240);
+    pakeitimasPo16bitus(bitai, 32, 224);
+    pakeitimasPo16bitus(bitai, 48, 208);
+    pakeitimasPo16bitus(bitai, 64, 192);
+    pakeitimasPo16bitus(bitai, 80, 176);
+    pakeitimasPo16bitus(bitai, 96, 160);
+    pakeitimasPo16bitus(bitai, 112, 144);
+
+    for (int i = 0; i < binarinis_ilgis / 2; i++) {
+        if (bitai[i] == bitai[binarinis_ilgis - i - 1]) { 
+            bitai[i] = 1;
+        } else {
+            bitai[i] = 0; 
+        }
+    }
+}
+
+string hashFunkcija(string simboliu_seka) {
+        int ascii_suma = 0; 
+        int daugiklis = 1; 
+    
+        int ilgis = 8 * simboliu_seka.size(); 
+
+        // Zodis isverciamas i ASCII
+        for (char simbolis : simboliu_seka) {
+            ascii_suma += (int)simbolis * daugiklis;
+            daugiklis++;
+        }
+
+        // Ascii_suma isverciama i binary
+        bitset<256> binarinis_kodas((int)ascii_suma);
+        for (int i = 0; i < 3; i++)
+        {
+            bitaiKeiciasiVietomis(binarinis_kodas);
+        }
+        
+        string hexKodas = hexPadarymas(binarinis_kodas);
+        //cout << "Hash kodo atvaizdavimas hex pavidalu: " << hex << hexKodas << endl;
+        return hexKodas;
+}
+
+    /////VARTOTOJAI/////
+
+void generuotiVartotojus(vector<Vartotojas>& vartotojai, ofstream& fail) {
+    for (int i = 0; i < 1000; i++) {
+        string vardas = randomVardas();
+        string viesasis_raktas = hashFunkcija(vardas + to_string(i));
+        int balansas = rand() % 1000000 + 100;
+        
+        vartotojai.push_back({vardas, viesasis_raktas, balansas});
+    }
+
+    for (const auto& vartotojas : vartotojai) {
+        fail << "Vardas: " << vartotojas.vardas << endl;
+        fail << "Viesasis raktas: " << vartotojas.viesasis_raktas << endl;
+        fail << "Balansas: " << vartotojas.balansas << endl;
+        fail << "" << endl;
+    }
+    fail.close();
+}
+
+    /////TRANSAKCIJOS/////
+
+void generuotiTransakcijas(vector<Transakcija>& transakcijos, vector<Vartotojas>& vartotojai, ofstream& failas) {
+    for (int i = 0; i < 10000; i++) {
+        
+        // Siuntejas ir jo viesasis raktas
+        int siuntejas;
+        do {
+            siuntejas = rand() % vartotojai.size();
+        } while (vartotojai[siuntejas].balansas < 1);
+
+        string siuntejo_viesasis_raktas = vartotojai[siuntejas].viesasis_raktas;
+
+        // Gavejas ir jo viesasis raktas
+        int gavejas;
+        do {
+            gavejas = rand() % vartotojai.size();
+        } while (siuntejas == gavejas);
+
+        string gavejo_viesasis_raktas = vartotojai[gavejas].viesasis_raktas;
+
+        // Suma, kuri yra pervedama
+        int suma = rand() % 10000 + 1;
+
+        // Transakcijos unikalusis kodas
+        string transakcijos_id = hashFunkcija(siuntejo_viesasis_raktas + gavejo_viesasis_raktas + to_string(suma));
+
+        // Patikriname, ar uztenka siuntejo balanso transakcijai vykdyti
+        if (suma > vartotojai[siuntejas].balansas) {
+            cout << "Klaida!" << endl;
+            cout << "Transakcijos ID: " << transakcijos_id << endl;
+            cout << "Siuntejo viesasis raktas: " << siuntejo_viesasis_raktas << endl;
+            cout << "Gavejo viesasis raktas: " << gavejo_viesasis_raktas << endl;
+            cout << "Suma: " << suma << endl; 
+            cout << "Siuntejo balanso nepakanka transakcijai vykdyti." << endl;
+            cout << "" << endl;
+            continue; 
+        }
+
+        // Pakeiciame siunteju ir gaveju balansus
+        vartotojai[siuntejas].balansas -= suma;
+        vartotojai[gavejas].balansas += suma;
+
+        transakcijos.push_back({transakcijos_id, siuntejo_viesasis_raktas, gavejo_viesasis_raktas, suma});
+    }
+
+    for (const auto& transakcija : transakcijos) {
+        failas << "Transakcijos ID: " << transakcija.transakcijos_id << endl;
+        failas << "Siuntejo viesasis raktas: " << transakcija.siuntejo_viesasis_raktas << endl;
+        failas << "Gavejo viesasis raktas: " << transakcija.gavejo_viesasis_raktas << endl;
+        failas << "Suma: " << transakcija.suma << endl;
+        failas << "" << endl;
+    }
+    failas.close();
+}
+
+    /////BLOKAI/////
+
+const int DifficultyTarget = 1;
+
+int pridetiNonce(Blokas& blokas) {
+    int nonce = 0;
+    string hashas;
+    do {
+        nonce++;
+        hashas = hashFunkcija(blokas.bloko_id + to_string(nonce));
+    } while (hashas.substr(0, DifficultyTarget) != string(DifficultyTarget, '0'));
+
+    blokas.nonce = nonce;
+    blokas.bloko_id = hashas;
+    return nonce;
+}
+
+void generuotiBlokus(vector<Blokas>& blokai, vector<Transakcija>& transakcijos, ofstream& failiukas) {
+    char pasirinkimas = 't';
+    
+    while (pasirinkimas == 't' && !transakcijos.empty()) {
+        Blokas naujas_blokas;
+        vector<Transakcija> isrinktos_transakcijos;
+        vector<string> transakciju_unikalus_kodas;
+
+        for (int j = 0; j < 100 && !transakcijos.empty(); j++) {
+            int randomTransakcijosIndeksas = rand() % transakcijos.size();
+            isrinktos_transakcijos.push_back(transakcijos[randomTransakcijosIndeksas]);
+            transakciju_unikalus_kodas.push_back(transakcijos[randomTransakcijosIndeksas].transakcijos_id);
+            
+            // Pasalinama transakcija, kuri jau buvo panaudota. Tam, kad nenaudoti ja kelis kartus
+            transakcijos.erase(transakcijos.begin() + randomTransakcijosIndeksas);
+        }
+
+        string sujungtasTransakcijuID;
+        for (const auto& id : transakciju_unikalus_kodas) {
+            sujungtasTransakcijuID += id;
+        }
+
+        naujas_blokas.bloko_id = hashFunkcija(sujungtasTransakcijuID);
+        naujas_blokas.transakcijos = isrinktos_transakcijos;
+
+        failiukas << "Iskastas blokas " << (blokai.size() + 1) << endl;
+        pridetiNonce(naujas_blokas);
+        
+        // Pridedame naujus blokus i bloku sarasa
+        blokai.push_back(naujas_blokas);
+
+        failiukas << "Bloko ID: " << naujas_blokas.bloko_id << endl;
+        failiukas << "Nonce: " << naujas_blokas.nonce << endl;
+        failiukas << "Transakcijos: " << endl;
+        failiukas << "_______________________________________________________________________________________" << endl;
+        failiukas << " " << endl;
+        for (const auto& tr : naujas_blokas.transakcijos) {
+            failiukas << "Transakcijos ID: " << tr.transakcijos_id << endl;
+            failiukas << "Siuntejo viesasis raktas: " << tr.siuntejo_viesasis_raktas << endl;
+            failiukas << "Gavejo viesasis raktas: " << tr.gavejo_viesasis_raktas << endl;
+            failiukas << "Suma: " << tr.suma << endl;
+            failiukas << "" << endl;
+        }
+        
+        cout << "Ar norite sukurti dar viena bloka? (t/n): ";
+        cin >> pasirinkimas;
+        if (pasirinkimas != 't') break;
+    }
+
+    failiukas.close();
+}
+
+void issaugotiBalansus(vector<Vartotojas>& vartotojai) {
+    ofstream balansu_failas("NaujiVartotojuBalansai.txt");
+    for (const auto& vartotojas : vartotojai) {
+        balansu_failas << "Vardas: " << vartotojas.vardas << endl;
+        balansu_failas << "Viesasis raktas: " << vartotojas.viesasis_raktas << endl;
+        balansu_failas << "Balansas: " << vartotojas.balansas << endl;
+        balansu_failas << "" << endl;
+    }
+    balansu_failas.close();
+}
+
+// NUSKAITOMI VARTOTOJAI //
+void nuskaitytiVartotojus(vector<Vartotojas>& vartotojai) {
+    ifstream fail("Vartotojai.txt");
+    if (!fail.is_open()) {
+        cout << "Nepavyko atidaryti 'Vartotojai.txt' failo." << endl;
+        return;
+    }
+
+    Vartotojas vartotojas;
+    while (getline(fail, vartotojas.vardas)) {
+        getline(fail, vartotojas.viesasis_raktas);
+        fail >> vartotojas.balansas;
+        fail.ignore();
+        vartotojai.push_back(vartotojas);
+    }
+
+    fail.close();
+}
+
+// NUSKAITOMOS TRANSAKCIJOS //
+void nuskaitytiTransakcijas(vector<Transakcija>& transakcijos) {
+    ifstream fail("Transakcijos.txt");
+    if (!fail.is_open()) {
+        cout << "Nepavyko atidaryti 'Transakcijos.txt' failo." << endl;
+        return;
+    }
+
+    Transakcija transakcija;
+    while (getline(fail, transakcija.transakcijos_id)) {
+        getline(fail, transakcija.siuntejo_viesasis_raktas);
+        getline(fail, transakcija.gavejo_viesasis_raktas);
+        fail >> transakcija.suma;
+        fail.ignore();
+        transakcijos.push_back(transakcija);
+    }
+
+    fail.close();
+}
+
+// NUSKAITOMI BLOKAI //
+void nuskaitytiBlokus(vector<Blokas>& blokai) {
+    ifstream fail("Blokai.txt");
+    if (!fail.is_open()) {
+        cout << "Nepavyko atidaryti 'Blokai.txt' failo." << endl;
+        return;
+    }
+
+    Blokas blokas;
+    while (getline(fail, blokas.bloko_id)) {
+        string line;
+        while (getline(fail, line) && !line.empty()) {
+            Transakcija transakcija;
+            transakcija.transakcijos_id = line;
+            getline(fail, transakcija.siuntejo_viesasis_raktas);
+            getline(fail, transakcija.gavejo_viesasis_raktas);
+            fail >> transakcija.suma;
+            fail.ignore();
+            blokas.transakcijos.push_back(transakcija);
+        }
+        blokai.push_back(blokas);
+    }
+
+    fail.close();
+}
